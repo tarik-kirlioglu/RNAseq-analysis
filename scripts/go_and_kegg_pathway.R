d3f1aff394ad52e34caf4adb1fc5ecbf570faf19
Tarık Kırlıoğlu <149543725+tarik-kirlioglu@users.noreply.github.com>
2024-08-18 19:53:19 +0300

    Create go_and_kegg_pathway.R

---
+++ scripts/go_and_kegg_pathway.R
@@ -0,0 +1,42 @@
+#importing libraries
+library(clusterProfiler)
+library(pathview)
+library(enrichplot)
+
+#reading results
+res <- read.csv("res_and_normalized.csv", header = T, row.names = 2)
+
+#selecting significant genes
+significant_genes <- res %>% 
+  filter(padj <= 0.05 & abs(log2FoldChange) > 1)
+
+#getting ensembl ids
+genelist <- rownames(significant_genes)
+#enrichment gene ontology analysis
+ego <- enrichGO(gene = genelist,
+                OrgDb = "org.Hs.eg.db",
+                keyType = "ENSEMBL",
+                ont = "BP",
+                readable = T)
+#plotting ego
+barplot(ego)
+
+#getting uniprot ids
+
+uniprotid <- significant_genes$uniprot_id
+
+#searching kegg organism code
+homo_sapiens <- search_kegg_organism("Homo sapiens", by="scientific_name")
+
+#enrichment kegg pathway analysis
+ekegg <- enrichKEGG(gene = uniprotid,
+                   keyType = "uniprot",
+                   organism = "hsa")
+
+#visualization
+browseKEGG(ekegg, pathID = "hsa04080")
+
+hsa04080 <- pathview(gene.data  = uniprotid,
+                     pathway.id = "hsa04080",
+                     species    = "hsa",
+                     gene.idtype = "uniprot")
