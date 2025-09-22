642ca7120f9f0b273fea5663592c2bc289517e4a
Tarık Kırlıoğlu <tarik.kirliogllu@gmail.com>
2025-09-22 22:45:42 +0300

    Update rnaseq_pipeline.sh

--- scripts/rnaseq_pipeline.sh
+++ scripts/rnaseq_pipeline.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-counts="/mnt/c/users/honor/desktop/homo_sapiens/counts/"
-fastqc="/mnt/c/users/honor/desktop/homo_sapiens/fastqc/"
-genome_index="/mnt/c/users/honor/desktop/homo_sapiens/genome_index/"
-mapped="/mnt/c/users/honor/desktop/homo_sapiens/mapped/"
-reads="/mnt/c/users/honor/desktop/homo_sapiens/reads/"
-trimmed="/mnt/c/users/honor/desktop/homo_sapiens/trimmed/"
-multiqc="/mnt/c/users/honor/desktop/homo_sapiens/multiqc/"
+counts="/mnt/c/users/honor/desktop/homo_sapiens/counts"
+fastqc="/mnt/c/users/honor/desktop/homo_sapiens/fastqc"
+genome_index="/mnt/c/users/honor/desktop/homo_sapiens/genome_index"
+mapped="/mnt/c/users/honor/desktop/homo_sapiens/mapped"
+reads="/mnt/c/users/honor/desktop/homo_sapiens/reads"
+trimmed="/mnt/c/users/honor/desktop/homo_sapiens/trimmed"
+multiqc="/mnt/c/users/honor/desktop/homo_sapiens/multiqc"
 
 echo "Step1:Prefetch SRA Data"
 
-prefetch -O ${reads} PRJNA694054
+prefetch -O ${reads}/ PRJNA694054
 
 echo "Step2:Extract fastq Files"
 
-fasterq-dump ${reads}*.sra -O ${reads}
+fasterq-dump ${reads}/*.sra -O ${reads}/
 
 echo "Step3:FastQC"
 
-fastqc ${reads}*.fastq -o ${fastqc}
+fastqc ${reads}/*.fastq -o ${fastqc}/
 
 echo "Step4:MultiQC"
 
-multiqc ${fastqc}*_fastqc* -o ${multiqc}
+multiqc ${fastqc}/*_fastqc* -o ${multiqc}/
 
 echo "Step5:Trimming"
 
-for infile in ${reads}*_1.fastq
+for infile in ${reads}/*_1.fastq
 do
 	base=$(basename ${infile} _1.fastq)
 	java -jar /mnt/c/users/honor/desktop/software/Trimmomatic/dist/jar/trimmomatic-0.40-rc1.jar PE \
-				${infile} ${reads}${base}_2.fastq \
+				${infile} ${reads}/${base}_2.fastq \
                 ${base}_1.trim.fastq ${base}_1.untrim.fastq \
                 ${base}_2.trim.fastq ${base}_2.untrim.fastq \
                 SLIDINGWINDOW:4:20 MINLEN:25 \
@@ -40,7 +40,7 @@ done
 
 #moving trimmed files
 
-mv *.trim.* ${trimmed}
+mv *.trim.* ${trimmed}/
 
 #downloading gtf and genom fasta files
 
@@ -50,20 +50,20 @@ wget https://ftp.ensembl.org/pub/release-112/gtf/homo_sapiens/Homo_sapiens.GRCh3
 echo "Step6:STAR Genome Index"
 
 STAR --runMode genomeGenerate \
-	 --genomeDir ${genome_index} \
+	 --genomeDir ${genome_index}/ \
 	 --genomeFastaFiles Homo_sapiens.GRCh38.dna.primary_assembly.fa \
 	 --sjdbGTFfile Homo_sapiens.GRCh38.112.gtf \
 	 --runThreadN 8
 
 echo "Step7:STAR Mapping"
 
-for infile in ${trimmed}*_1.trim.fastq
+for infile in ${trimmed}/*_1.trim.fastq
 do
    base=$(basename ${infile} _1.trim.fastq)
-   STAR --genomeDir ${genome_index} \
+   STAR --genomeDir ${genome_index}/ \
 	--runThreadN 8 \
-	--readFilesIn ${infile} ${trimmed}${base}_2.trim.fastq \
-	--outFileNamePrefix ${mapped}${base} \
+	--readFilesIn ${infile} ${trimmed}/${base}_2.trim.fastq \
+	--outFileNamePrefix ${mapped}/${base} \
 	--outSAMtype BAM SortedByCoordinate \
 	--quantMode GeneCounts \
 	--outSAMattributes Standard
@@ -71,4 +71,4 @@ done
 
 echo "Step8:Calculating counts with featureCounts"
 
-featureCounts -a Homo_sapiens.GRCh38.112.gtf  -o gene_counts.out -T 8 -p  ${mapped}*.bam
+featureCounts -a Homo_sapiens.GRCh38.112.gtf  -o gene_counts.out -T 8 -p  ${mapped}/*.bam
